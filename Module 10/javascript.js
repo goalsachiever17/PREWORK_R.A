89683b994f573b248743406722bab33cbc4dfc74
goalsachiever17 <apobecky@gmail.com>
2018-01-24 15:18:51 -0600

    first commit

---
+++ Module 10/javascript.js
@@ -0,0 +1,30 @@
+
+         //when the boxGrow button is pressed...
+          document.getElementById("button1").addEventListener("click", function(){
+          //increase the box.
+            document.getElementById("box").style.height = "250px"; 
+
+
+             });
+
+       
+          //when the boxBlue button is pressed...
+          document.getElementById("button2").addEventListener ("click", function(){
+
+          //change the box to blue.
+
+         document.getElementById("box").style.backgroundcolor = "blue";
+          });
+
+
+
+            
+          document.getElementById("button4").addEventListener("click", function(){
+            
+            document.getElementById("box").style.height = "150px";
+
+          });
+             
+
+
+        
